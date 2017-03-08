94c0cb806215116f61acb958601557102a4b3cc4
playbar <hgl868@126.com>
2017-03-08 10:38:04 +0800

    添加工程代码

---
+++ hello-jni/app/src/main/java/com/bar/test/ImgLoaderTest.java
@@ -0,0 +1,28 @@
+package com.bar.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.BounceInterpolator;
+import android.widget.Toast;
+
+import com.guardanis.imageloader.ImageRequest;
+import com.guardanis.imageloader.transitions.modules.ScalingTransitionModule;
+import com.guardanis.imageloader.transitions.modules.TransitionModule;
+import com.makeramen.roundedimageview.CircleMenuLayout;
+import com.makeramen.roundedimageview.CircleMenuLayout.OnMenuItemClickListener;
+
+
+public class ImgLoaderTest extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+
+		
+	}
+
+}
