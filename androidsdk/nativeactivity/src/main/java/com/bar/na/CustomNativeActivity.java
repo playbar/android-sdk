b075c98acac74eb572d3cddb4fff51f965238bc3
playbar <hgl868@126.com>
2017-03-09 10:18:34 +0800

    添加代码

---
+++ androidsdk/nativeactivity/src/main/java/com/bar/na/CustomNativeActivity.java
@@ -0,0 +1,12 @@
+package com.bar.na;
+
+import android.app.NativeActivity;
+import android.os.Bundle;
+
+public class CustomNativeActivity extends NativeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
