5ef32faac03796b97ed27677ab1b24c0894f088b
playbar <hgl868@126.com>
2017-03-17 16:34:14 +0800

    添加customfastscrollview

---
+++ androidsdk/samples/src/main/java/com/bar/customfastscrollview/Function.java
@@ -0,0 +1,14 @@
+package com.bar.customfastscrollview;
+
+/**
+ * Simple Function, designed after Google Guava's Function object.
+ * @author nolan
+ *
+ * @param <E>
+ * @param <T>
+ */
+public interface Function<E,T> {
+	
+	public T apply(E input);
+
+}
